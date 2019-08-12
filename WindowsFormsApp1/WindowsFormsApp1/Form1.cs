067a0c10911cd811e60cb453f1fccc4bf242e0ed
Elias-HUB <53981417+Elias-HUB@users.noreply.github.com>
2019-08-11 23:10:08 -0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Menu : Form
+	{
+		public Menu()
+		{
+			InitializeComponent();
+		}
+
+		private void ValNumeros(object sender, KeyPressEventArgs e)
+		{
+			if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+			{
+				e.Handled = true;
+			}
+			if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+
+			{ e.Handled = true;
+				return;
+			}
+		}
+
+			private void ValLetras(object sender, KeyPressEventArgs e)
+		{
+
+			if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+			{
+				e.Handled = true;
+				return;
+			}
+		}
+		private void ValLetNum(object sender, KeyPressEventArgs e)
+		{
+			if (!(char.IsNumber(e.KeyChar)) && !(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back) && (e.KeyChar != 32))
+			{
+				e.Handled = true;
+				return;
+			}
+		}
+	}
+}
